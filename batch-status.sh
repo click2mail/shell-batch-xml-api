ffde26a7bf5dafb2eb4d622ea72563147e6d0c1e
jclaus <jclaus@click2mail.com>
2013-03-13 12:32:25 -0400

    Adding scripts and config files

---
+++ batch-status.sh
@@ -0,0 +1,33 @@
+#/!bin/bash
+
+env=$1
+batchID=$2
+
+echo
+echo Environment is: $env
+
+if [ "$env" == "dev" ] || [ "$env" == "stage" ]; then
+	env="$env-batch"
+else
+	env="batch"
+fi
+
+if [ "$batchID" == "" ] || [ "$batchID" == null ]; then
+	echo Batch ID is not supplied
+	exit
+fi
+
+
+echo
+echo Batch ID is: $batchID
+
+#Check the status of the batch job
+temp="curl https://<username>:<password>@$env.click2mail.com/v1/batches/$batchID"
+
+echo
+echo Running:
+echo $temp
+
+temp=`curl https://<username>:<password>@$env.click2mail.com/v1/batches/$batchID`
+
+echo $temp
---
+++ test-batch.sh
@@ -0,0 +1,95 @@
+#/!bin/bash
+
+configFile="batch-config.ini"
+
+
+env=$1
+batchID=-1				        #Batch ID
+jobXML="null"                     	        #Batch XML
+jobDOC="null"                      		#Job Doc
+temp=""
+
+echo
+echo Environment: $env
+
+
+if [ "$env" == "dev" ] || [ "$env" == "stage" ]; then
+	env="$env-batch"
+else
+	echo Did you mean to submit to production \(yes/no\)?
+	read answer
+	if [ $answer == "yes" ]; then
+		env="batch"
+	else
+		echo Exiting...
+		exit 2
+	fi
+fi
+
+
+while read LINE
+do
+	if [[ "$LINE" =~ "xml" ]]; then
+		jobXML=${LINE:5}
+	fi
+	if [[ "$LINE" =~ "pdf" ]]; then
+		jobDOC=${LINE:5}
+	fi
+done < $configFile
+
+
+jobXML=`echo $jobXML | tr -s " "`
+jobDOC=`echo $jobDOC | tr -s " "`
+
+jobXML="@$jobXML"
+jobDOC="@$jobDOC"
+
+echo
+echo Job XML: $jobXML
+
+echo
+echo Job Doc: $jobDOC
+
+
+
+
+#Create batch order and get batch id
+temp=`curl -X POST https://<username>:<password>@$env.click2mail.com/v1/batches`
+
+arr=`echo $temp | sed 's/</ /g'`
+
+i=0
+
+for x in $arr
+do
+	if [ "$i" -eq 11 ]; then
+		batchID=$x
+	fi
+	i=$(($i + 1))
+done
+
+
+echo
+batchID=${batchID:3}
+echo Batch ID = $batchID
+
+
+
+#Upload the batch XML
+temp=`curl -X PUT https://<username>:<password>@$env.click2mail.com/v1/batches/$batchID -H "Content-Type: application/xml" --data-binary $jobXML`
+echo
+echo Response from XML upload: $temp
+
+
+#Upload PDF
+temp=`curl -X PUT https://<username>:<password>@$env.click2mail.com/v1/batches/$batchID -H "Content-Type: application/pdf" --data-binary $jobDOC`
+
+echo
+echo Response from PDF upload: $temp
+
+
+#Submit the job
+temp=`curl -X POST https://<username>:<password>@$env.click2mail.com/v1/batches/$batchID`
+
+echo
+echo Response after job submit: $temp
